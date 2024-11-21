f2e376a69a4a3159eb9dba1f403725a1b99a1bf7
Pablo Cacciagioni <pablo1234caccia@gmail.com>
2024-11-21 11:41:27 -0300

    Implementing the token

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,22 +1,25 @@
 from django.shortcuts import render
 from .models import *
-from rest_framework import generics
-from rest_framework import viewsets
+from rest_framework import generics, viewsets
 from .serializers import *
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
 
 class MenuItemView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Menu.objects.all()
     serializer_class = MenuItemSerializer
     
 class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Menu.objects.all()
     serializer_class = MenuItemSerializer
     
 
 class BookingViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
